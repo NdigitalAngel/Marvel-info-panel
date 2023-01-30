d17c9daf0514ac244c552ed2bea7c29d11f99ad4
ivan <defvermillion@gmail.com>
2023-01-30 13:33:24 +0300

    initial commit

---
+++ src/components/errorMessage/ErrorMessage.js
@@ -0,0 +1,9 @@
+import img from './1479.gif';
+
+const ErrorMessage = () => {
+    return (
+        <img src={img} alt="error" style={{ display: 'block', margin: '0 auto', width: '75px', height: '75px'}} />
+    )
+}
+
+export default ErrorMessage;
