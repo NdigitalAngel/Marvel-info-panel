4025cf9ec25e28ff09e353fac6f578b64ce60862
ivan <defvermillion@gmail.com>
2023-02-28 18:30:53 +0300

    add react-router

--- src/components/appHeader/AppHeader.js
+++ src/components/appHeader/AppHeader.js
@@ -1,18 +1,19 @@
 import './appHeader.scss';
+import { Link, NavLink } from "react-router-dom";
 
 const AppHeader = () => {
     return (
         <header className="app__header">
             <h1 className="app__title">
-                <a href="1">
+                <Link to='/'>
                     <span>Marvel</span> information portal
-                </a>
+                </Link>
             </h1>
             <nav className="app__menu">
                 <ul>
-                    <li><a href="2">Characters</a></li>
+                    <li><NavLink end activeStyle={{'color': '#9f0013'}} to="/">Characters</NavLink></li>
                     /
-                    <li><a href="3">Comics</a></li>
+                    <li><NavLink end activeStyle={{'color': '#9f0013'}} to="/comics">Comics</NavLink></li>
                 </ul>
             </nav>
         </header>
